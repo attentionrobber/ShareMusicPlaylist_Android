49717d091461bc4dcdef12083dbfbbf361c8eea5
attentionrobber <lorch987@gmail.com>
2017-03-27 22:20:52 +0900

    gradle 설정 추가(dependencies 등)

--- app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainActivity.java
+++ app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainActivity.java
@@ -1,13 +1,22 @@
 package com.hyunseok.android.sharemusicplaylist;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @ViewById
+    TextView textView;
+    @ViewById
+    Button button;
+
+    public void button_test(View view) {
+        textView.setText("Is it well?");
     }
 }
