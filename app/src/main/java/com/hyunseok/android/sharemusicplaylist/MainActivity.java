8de0c4edb0fbab366d89dfc97a669d1a254bb514
attentionrobber <lorch987@gmail.com>
2017-03-27 22:40:40 +0900

    Android Annotation Test

--- app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainActivity.java
+++ app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainActivity.java
@@ -2,12 +2,19 @@ package com.hyunseok.android.sharemusicplaylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @ViewById
+    TextView textView;
+
+    public void button_test(View view) {
+        textView.setText("Is it work well????");
     }
 }
