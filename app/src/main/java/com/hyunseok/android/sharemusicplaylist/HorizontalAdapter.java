852b1b58e61bde04683957572793cd6a8b2e44e9
attentionrobber <lorch987@gmail.com>
2017-03-28 19:27:04 +0900

    RecyclerView Test

---
+++ app/src/main/java/com/hyunseok/android/sharemusicplaylist/HorizontalAdapter.java
@@ -0,0 +1,63 @@
+package com.hyunseok.android.sharemusicplaylist;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017-03-28.
+ */
+public class HorizontalAdapter extends RecyclerView.Adapter<HorizontalAdapter.Holder> {
+
+    private Context context;
+    private List<String> horizontalList;
+
+    public HorizontalAdapter(Context context, List<String> horizontalList) {
+        this.context = context;
+        this.horizontalList = horizontalList;
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false); // context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) 와 같다.
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+
+        holder.tv_recycler.setText(horizontalList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return horizontalList.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+
+        ImageView imageView_recycler;
+        TextView tv_recycler;
+
+        public Holder(View itemView) {
+            super(itemView);
+            imageView_recycler = (ImageView) itemView.findViewById(R.id.imageView_recycler);
+            tv_recycler = (TextView) itemView.findViewById(R.id.tv_recycler);
+
+            tv_recycler.setOnClickListener(clickListener);
+        }
+
+        private View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+    }
+}
--- app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainActivity.java
+++ app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainActivity.java
@@ -1,20 +1,63 @@
 package com.hyunseok.android.sharemusicplaylist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.StackView;
 import android.widget.TextView;
 
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    private List<String> horizontalList;
+    private HorizontalAdapter horizontalAdapter;
+
     @ViewById
-    TextView textView;
+    RecyclerView recyclerView_horizon;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        horizontalList = new ArrayList<>();
+        horizontalList.add("horizontal 1");
+        horizontalList.add("horizontal 2");
+        horizontalList.add("horizontal 3");
+        horizontalList.add("horizontal 4");
+        horizontalList.add("horizontal 5");
+        horizontalList.add("horizontal 6");
+        horizontalList.add("horizontal 7");
+        horizontalList.add("horizontal 8");
+        horizontalList.add("horizontal 9");
+        horizontalList.add("horizontal 10");
+
+        horizontalAdapter = new HorizontalAdapter(this, horizontalList);
+
+        recyclerView_horizon.setAdapter(horizontalAdapter);
+        LinearLayoutManager horizontalLayoutManager
+                = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView_horizon.setLayoutManager(horizontalLayoutManager);
+
+    }
+
+
 
     public void button_test(View view) {
-        textView.setText("Is it work well????");
+        //textView.setText("Is it ");
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
