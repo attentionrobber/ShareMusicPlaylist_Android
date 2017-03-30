c6c5b5d196fad6861be0a326fab226ed0ce4d514
SOUTHNINE\kang <kangnk5@gmail.com>
2017-03-30 11:47:39 +0900

    Rest API(Retrofit) interface init

---
+++ app/src/main/java/com/hyunseok/android/sharemusicplaylist/retrofit_interface/RestApiService.java
@@ -0,0 +1,25 @@
+package com.hyunseok.android.sharemusicplaylist.retrofit_interface;
+
+import com.hyunseok.android.sharemusicplaylist.domain.User;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by kang on 2017-03-30.
+ */
+
+public interface RestApiService {
+    @POST("")
+    Call<User> getUserPassword(@Query("email") String email);
+    @POST("")
+    Call<User> setUserData(@Query("email") String email,
+                           @Query("password") String password,
+                           @Query("name") String name,
+                           @Query("gender") String gender,
+                           @Query("age") Integer age
+                            );
+}
