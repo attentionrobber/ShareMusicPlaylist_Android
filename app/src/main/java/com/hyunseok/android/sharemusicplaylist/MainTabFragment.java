e727a52fb6e6b151bb3916a2177efdb33064ad9a
SOUTHNINE\kang <kangnk5@gmail.com>
2017-04-05 21:31:47 +0900

    Search Tab Layout 수정(검색결과가 새로운 Activity가 아닌 View로 처리), SearchFunction Class 추가(init Search 부분 Class로 작성.)

--- app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainTabFragment.java
+++ app/src/main/java/com/hyunseok/android/sharemusicplaylist/MainTabFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -38,7 +39,8 @@ public class MainTabFragment extends Fragment {
     private int layout;
 
     // Search Tab
-    RecyclerView recyclerView_horizon;
+    RecyclerView recyclerView_horizon_Latest;
+    RecyclerView recyclerView_horizon_Best;
     HorizontalAdapter horizontalAdapter;
 
     List<String> horizontalList;
@@ -60,7 +62,7 @@ public class MainTabFragment extends Fragment {
     List<String> playlistDatas;
 
     FloatingActionButton btn_newList;
-
+    FragmentManager fm;
 
     public MainTabFragment() {
         // Required empty public constructor
@@ -77,7 +79,7 @@ public class MainTabFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        fm = getFragmentManager();
         if (getArguments() != null) {
             mTabType = getArguments().getString(ARG_TAB_TYPE);
 
@@ -118,27 +120,8 @@ public class MainTabFragment extends Fragment {
     }
 
     private void init_searchTab(View view) {
-        recyclerView_horizon = (RecyclerView) view.findViewById(R.id.recyclerView_horizon);
-        btn_search = (ImageButton) view.findViewById(R.id.imgbtn_search);
-        btn_search.setOnClickListener(clickListener);
-
-        horizontalList = new ArrayList<>();
-        horizontalList.add("horizontal 1");
-        horizontalList.add("horizontal 2");
-        horizontalList.add("horizontal 3");
-        horizontalList.add("horizontal 4");
-        horizontalList.add("horizontal 5");
-        horizontalList.add("horizontal 6");
-        horizontalList.add("horizontal 7");
-        horizontalList.add("horizontal 8");
-        horizontalList.add("horizontal 9");
-        horizontalList.add("horizontal 10");
-
-        horizontalAdapter = new HorizontalAdapter(getContext(), horizontalList);
-        LinearLayoutManager horizontalLayoutManager
-                = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        recyclerView_horizon.setLayoutManager(horizontalLayoutManager);
-        recyclerView_horizon.setAdapter(horizontalAdapter);
+        SearchFunction searchFunction = new SearchFunction(view, getContext());
+        searchFunction.init_searchTab();
     }
 
     private void init_playerTab(View view) {
@@ -247,4 +230,5 @@ public class MainTabFragment extends Fragment {
         super.onDetach();
         //Logger.print("MainTabFragment", "=================onDetach===================");
     }
+
 }
---
+++ app/src/main/java/com/hyunseok/android/sharemusicplaylist/SearchFunction.java
@@ -0,0 +1,113 @@
+package com.hyunseok.android.sharemusicplaylist;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.NoCopySpan;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.hyunseok.android.sharemusicplaylist.adapter.HorizontalAdapter;
+
+import org.androidannotations.annotations.EFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kang on 2017-04-05.
+ */
+
+public class SearchFunction {
+    private LinearLayout recommend_layout,searchResult_layout;
+    private RecyclerView recyclerView_horizon_Latest;
+    private RecyclerView recyclerView_horizon_Best;
+    private ImageButton btn_search;
+    private EditText et_search;
+    View view;
+    private Context context;
+    private int status = View.GONE;
+
+    public SearchFunction(View view,Context context){
+        this.view = view;
+        this.context = context;
+    }
+    public void init_widget(){
+        et_search = (EditText)view.findViewById(R.id.et_search);
+        recyclerView_horizon_Latest = (RecyclerView)view.findViewById(R.id.recyclerView_horizon_latest);
+        recyclerView_horizon_Best = (RecyclerView)view.findViewById(R.id.recyclerView_horizon_best);
+        btn_search = (ImageButton)view.findViewById(R.id.imgbtn_search);
+        recommend_layout = (LinearLayout)view.findViewById(R.id.recommend_layout);
+        searchResult_layout= (LinearLayout)view.findViewById(R.id.result_layout);
+    }
+    public void init_searchTab() {
+        init_widget();
+        btn_search.setOnClickListener(clickListener);
+
+        List<String> horizontalList = new ArrayList<>();
+        horizontalList.add("horizontal 1");
+        horizontalList.add("horizontal 2");
+        horizontalList.add("horizontal 3");
+        horizontalList.add("horizontal 4");
+        horizontalList.add("horizontal 5");
+        horizontalList.add("horizontal 6");
+        horizontalList.add("horizontal 7");
+        horizontalList.add("horizontal 8");
+        horizontalList.add("horizontal 9");
+        horizontalList.add("horizontal 10");
+
+        HorizontalAdapter horizontalAdapter = new HorizontalAdapter(context, horizontalList);
+        LinearLayoutManager horizontalLayoutManager
+                = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+
+        HorizontalAdapter horizontalAdapter1 = new HorizontalAdapter(context, horizontalList);
+        LinearLayoutManager horizontalLayoutManager1
+                = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerView_horizon_Latest.setLayoutManager(horizontalLayoutManager);
+        recyclerView_horizon_Latest.setAdapter(horizontalAdapter);
+
+        recyclerView_horizon_Best.setLayoutManager(horizontalLayoutManager1);
+        recyclerView_horizon_Best.setAdapter(horizontalAdapter1);
+
+        et_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(et_search.getText().toString().equals("")){
+                    recommend_layout.setVisibility(View.VISIBLE);
+                    searchResult_layout.setVisibility(View.GONE);
+                }else{
+                    recommend_layout.setVisibility(View.GONE);
+                    searchResult_layout.setVisibility(View.VISIBLE);
+                    //TODO : RETROFIT & RECYCLERVIEW
+                }
+
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+    }
+
+    View.OnClickListener clickListener = v -> {
+
+        if(status == View.GONE)
+            status = View.VISIBLE;
+        else
+            status = View.GONE;
+
+        recommend_layout.setVisibility(status);
+    };
+
+}
